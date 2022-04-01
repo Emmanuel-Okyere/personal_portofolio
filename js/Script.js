7b6e4e97a40ed2cba958a5b4fe81c8cd83d1bbd8
Emmanuel-Okyere <57065066+Emmanuel-Okyere@users.noreply.github.com>
2022-04-01 16:35:22 +0000

    Added effects to the page

---
+++ js/Script.js
@@ -0,0 +1,38 @@
+const uni = document.getElementsByClassName("university")[0];
+const elp3 = document.getElementsByClassName("elipse3")[0];
+
+////
+const shs = document.getElementsByClassName("shs")[0];
+const elp4 = document.getElementsByClassName("elipse4")[0];
+///
+const jhs = document.getElementsByClassName("jhs")[0];
+const elp5 = document.getElementsByClassName("elipse5")[0];
+//
+const data = document.getElementsByClassName("datascience")[0];
+const elp6 = document.getElementsByClassName("elipse6")[0];
+//
+const deep = document.getElementsByClassName("deeplearning")[0];
+const elp7 = document.getElementsByClassName("elipse7")[0];
+//
+const machine = document.getElementsByClassName("machine")[0];
+const elp8 = document.getElementsByClassName("elipse8")[0];
+
+let defaultColor = "";
+function lightup(parent,child){
+    
+    parent.addEventListener("mouseenter",(e)=>{
+    defaultColor =  child.style.background;
+        child.style.background = "#E29014"
+    })
+
+    parent.addEventListener("mouseleave",(e)=>{
+        child.style.background = defaultColor
+    })
+}
+ 
+lightup(uni,elp3);
+lightup(shs, elp4);
+lightup(jhs, elp5);
+lightup(data, elp6);
+lightup(deep, elp7);
+lightup(machine, elp8);
