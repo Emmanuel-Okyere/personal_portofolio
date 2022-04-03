af8a9a108c790ca7a683b3a97173f815d25d2e2c
Emmanuel-Okyere <57065066+Emmanuel-Okyere@users.noreply.github.com>
2022-04-03 00:16:03 +0000

    Added the Project file

--- js/Script.js
+++ js/Script.js
@@ -16,7 +16,15 @@ const elp7 = document.getElementsByClassName("elipse7")[0];
 //
 const machine = document.getElementsByClassName("machine")[0];
 const elp8 = document.getElementsByClassName("elipse8")[0];
-
+//
+const amalitech = document.getElementsByClassName("amalitech")[0];
+const elp9 = document.getElementsByClassName("elipse9")[0];
+//
+const ugcs = document.getElementsByClassName("ugcs")[0];
+const elp10 = document.getElementsByClassName("elipse10")[0];
+//
+const gacl = document.getElementsByClassName("gacl")[0];
+const elp11 = document.getElementsByClassName("elipse11")[0];
 let defaultColor = "";
 function lightup(parent,child){
     
@@ -36,3 +44,7 @@ lightup(jhs, elp5);
 lightup(data, elp6);
 lightup(deep, elp7);
 lightup(machine, elp8);
+lightup(amalitech, elp9);
+lightup(ugcs, elp10);
+lightup(gacl, elp11);
+
